e1afda6c0424042cfd9f9403cdf6627d29e7c431
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-01-29 13:42:20 -0800

    Add variables to store user form selections when submitted.

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,10 @@ $(document).ready(function() {
     event.preventDefault();
     const userName = $("input#name").val();
     const question1Val = $("input:radio[name=question1]:checked").val();
+    const question2Val = $("input:radio[name=question2]:checked").val();
+    const question3Val = $("input:radio[name=question3]:checked").val();
+    const question4Val = $("input:radio[name=question4]:checked").val();
+    const question5Val = $("input:radio[name=question5]:checked").val();
+
   });
 });
